df98a3e93d0fc5bf543be1959c163f5bb5d4ac42
GijoMM <gijo.moura@gmail.com>
2019-10-30 15:28:11 -0700

    Updates

---
+++ precache-manifest.5b2574e2f4e71117fc16ae7b339c4755.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "984759d0f9daf079e029abc4e5f10f14",
+    "url": "/KittenFriends/index.html"
+  },
+  {
+    "revision": "539a39d4a9d9d27e97e4",
+    "url": "/KittenFriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "fc39004505c045d20e9f",
+    "url": "/KittenFriends/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "539a39d4a9d9d27e97e4",
+    "url": "/KittenFriends/static/js/2.761caa3a.chunk.js"
+  },
+  {
+    "revision": "fc39004505c045d20e9f",
+    "url": "/KittenFriends/static/js/main.d5f62516.chunk.js"
+  },
+  {
+    "revision": "e015d4dbda36c7efdd20",
+    "url": "/KittenFriends/static/js/runtime-main.fc5b7515.js"
+  }
+]);
